49f22b262149117af5726481c4417eb5091b02e2
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-24 11:32:09 +0300

    Aggiunta form in acquisti.php e varie funzioni in gestione.php

--- acquisti.php
+++ acquisti.php
@@ -6,6 +6,27 @@
     <title>acquisti.php</title>
 </head>
 <body>
-   <form action=""></form>
+   <form action="gestione.php" method="POST">
+    <p>Inserire nome dell'oggetto: </p>
+    <input type="text" name="nome">
+    <p>Inserire costo dell'oggetto: </p>
+    <input type="text" name="costo">
+    <p>selezionare la quantità dell'oggetto: </p>
+    <select name="quantita" id="quantita">
+        <option value="1">1</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="20">20</option>
+    </select>
+    <p>selezionare stato dell'oggetto: </p>
+    <input type="checkbox" name="stato" value="usato"> Usato
+    <p>selezionare pagamento desiderato:</p>
+    <input type="radio" name="pagamento" value="contanti"> Contanti
+    <input type="radio" name="pagamento" value="carta"> Carta
+    <input type="radio" name="pagamento" value="buoni"> Buono digitale
+    <br>
+    <br>
+    <input type="submit">
+   </form>
 </body>
 </html>
--- gestione.php
+++ gestione.php
@@ -7,6 +7,46 @@
 </head>
 <body>
     <?php
+    $name = $_POST["nome"];
+    $cost = $_POST["costo"];
+    $quantity = $_POST["quantita"];
+    $status = checkStatus();
+    $payment = $_POST["pagamento"];
+    $initialPrice = intval($cost) * intval($quantity);
+    $finalPrice = discount($status, $payment, $cost);
+    $listData = array("nome" => $name, "costo" => $cost, "quantita" => $quantity, "stato" => $status, "pagamento" => $payment, "Prezzo iniziale" => $initialPrice, "Prezzo finale" => $finalPrice);
+    createList($listData);
+
+
+    function checkStatus() {
+        if (isset($_POST["stato"])) {
+            return "Oggetto usato";
+        } else {
+            return "Oggetto nuovo";
+        }
+    }
+
+    function discount($s ,$p, $n){
+        if ($s == "Oggetto usato") {
+            $discount = ($n / 100) * 20;
+            $n -= $discount;
+        }
+        
+        if ($p == "carta") {
+            $n += 2;
+        }
+        return $n;
+    }
+
+    function createList($ld){
+        echo "<h1>DATI PRODOTTO:</h1>";
+        echo "<ul>";
+            foreach ($ld as $d => $value) {
+                echo "<li>" . $d . ": " . $value . "</li>";
+            }
+        echo "</ul>";
+    }
+
     ?>
 </body>
 </html>
